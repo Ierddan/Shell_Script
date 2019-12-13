00c6de07d3c488bedfabdfe2d52a2ebf989d6b91
Costa <ierddan.de.o.costa@accenture.com>
2019-12-13 13:06:37 -0300

    Scrip definição de variáveis, if e operador or(-o)

---
+++ Scripts/setVariaveis_ex1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+MEDMANHA="CARLOS"
+MEDTARDE="JOSÉ"
+MEDNOITE="JOÃO"
+
+echo "Bem vindo!"
+
+echo "Digite o turno que deseja para sua consulta (MANHA, TARDE OU NOITE):"
+read TURNO
+
+if [ $TURNO = "MANHA" -o "$TURNO" = "manha"  ]; then
+echo  "Médico(a) " $MEDMANHA " é o(a) único(a) a atender no turno da " $TURNO
+fi
+
+if [ "$TURNO" = "TARDE" -o "$TURNO" = "tarde" ]; then
+echo  "Médico(a) " $MEDTARDE " é o(a) único(a) a atender no turno da " $TURNO
+fi
+
+if [ "$TURNO" = "NOITE" -o "$TURNO" = "noite" ]; then
+echo "Médico(a) " $MEDNOITE " é o(a) único(a a atender no turno da " $TURNO
+fi
+
+sleep 2
+
+echo "Sua consulta foi agendada."
+sleep 4 
+
+exit
+
