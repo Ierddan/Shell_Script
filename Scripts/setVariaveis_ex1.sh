6bcc88e91dce21b9d1667f4470d329d137c8c187
Costa <ierddan.de.o.costa@accenture.com>
2019-12-13 13:08:01 -0300

    Adição de comentários

--- Scripts/setVariaveis_ex1.sh
+++ Scripts/setVariaveis_ex1.sh
@@ -9,6 +9,9 @@ echo "Bem vindo!"
 echo "Digite o turno que deseja para sua consulta (MANHA, TARDE OU NOITE):"
 read TURNO
 
+# -o = OR
+# -a = AND
+
 if [ $TURNO = "MANHA" -o "$TURNO" = "manha"  ]; then
 echo  "Médico(a) " $MEDMANHA " é o(a) único(a) a atender no turno da " $TURNO
 fi
